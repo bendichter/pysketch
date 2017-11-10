2f95259766e9c46118861108c686f0284129ac41
Ben Dichter <ben.dichter@gmail.com>
2017-11-10 16:59:26 +0800

    add grid and launcher app

--- pysketch.py
+++ pysketch.py
@@ -1,7 +1,8 @@
 import matplotlib.pyplot as plt
 from copy import copy
 
-class Pad:
+
+class NewPad:
     def __init__(self):
         self.pos = [0, 0]
         self.heading = 'up'
@@ -19,9 +20,16 @@ class Pad:
             new_pos[0] -= length
 
         self.ax.plot([self.pos[0], new_pos[0]], [self.pos[1], new_pos[1]],
-                 color=color, linewidth=linewidth, **kwargs)
+                     color=color, linewidth=linewidth, **kwargs)
         self.pos = new_pos
-        self.ax.axis('equal')
+        self.ax.axis('scaled')
+        self.ax.axis('off')
+
+        xl = self.ax.get_xlim()
+        yl = self.ax.get_ylim()
+        for x in range(int(xl[0]), int(xl[1])+1):
+            for y in range(int(yl[0]), int(yl[1])+1):
+                plt.scatter(x, y, 3, color='grey')
 
     def turn_left(self):
         if self.heading == 'up':
